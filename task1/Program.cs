a2eef3b15c45c06299f1dd92669d1c56194c7a29
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-13 19:45:56 +0300

    t1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumNumber(int num)
+{
+    if (num == 0) return 0;
+    else return (num % 10) + SumNumber(num / 10);
+}
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа = {SumNumber(number)}.");
