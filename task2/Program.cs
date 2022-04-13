815e269aa29618d5d5511cb5bca4777d27a8f5e8
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-13 19:54:02 +0300

    t2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.Write($"Введите первый параметр функции (m): ");
+long parameterM = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите второй параметр функции (n): ");
+long parameterN = Convert.ToInt32(Console.ReadLine());
+
+long AckermannFunction(long m, long n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+
+Console.WriteLine($"Функция Аккермана({parameterM}, {parameterN}) = {AckermannFunction(parameterM, parameterN)}.");
