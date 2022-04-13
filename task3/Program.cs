74771e8055b6de0e5dfa1ee17138c287ab82e7d4
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-13 19:59:58 +0300

    t3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Написать программу возведения числа А в целую стень B
+
+int RecPower(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * RecPower(a, b - 1);
+}
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите степень: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number}^{power} = {RecPower(number, power)}.");
